de615ce5e6a840779f567c1741e2ae4b8f44cae0
ullash4 <programmerullash@gmail.com>
2022-04-25 01:19:01 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './Pages/Header/Header';
 import Home from './Pages/Home/Home';
 import VideoPage from './Pages/VideoPage/VideoPage';
 
 function App() {
   return (
     <div>
-      
+      <Header></Header>
       <Routes>
-        <Route to="/" element={<Home></Home>}></Route>
-        <Route to='/cinematography' element={<VideoPage></VideoPage>}></Route>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path='/cinematography' element={<VideoPage></VideoPage>}></Route>
       </Routes>
     </div>
   );
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Carousel from '../Carousel/Carousel';
 import Gallery from '../Gallery/Gallery';
-import Header from '../Header/Header'
 
 const Home = () => {
     return (
         <div>
-          <Header></Header>
           <Carousel></Carousel>
           <Gallery></Gallery>
         </div>
